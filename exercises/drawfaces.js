c62f6d1c0767aed64743b970b4837999426bb53b
Bill Phan <billphan25@gmail.com>
2017-08-25 22:31:27 -0700

    Added exercises folder.

---
+++ exercises/drawfaces.js
@@ -0,0 +1,10 @@
+// Basic JavaScript function indicating structure and syntax.
+// Prints faces how many times input as an arguement.
+
+var drawFaces = function(howManyTimes) {
+  for (var i = 0; i < howManyTimes; i++) {
+    console.log(i + ":)");
+  }
+}
+
+drawFaces(10)
